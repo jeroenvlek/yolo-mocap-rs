bf34a38612b5139938c778f5448e65d5d2c6b57f
Jeroen Vlek <jeroen@perceptivebits.com>
2024-03-25 11:22:26 +0100

    handle different webcams

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
+use opencv::highgui;
 use opencv::prelude::*;
 use opencv::videoio;
-use opencv::highgui;
+use opencv::videoio::VideoCapture;
+use std::error::Error;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;  // 0 is the default camera
-    let opened = videoio::VideoCapture::is_opened(&cam)?;
-    if !opened {
-        panic!("Unable to open default camera!");
+const MAX_CAM_INDEX: i32 = 10;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let camera_index_list = list_cameras()?;
+    if camera_index_list.is_empty() {
+        panic!("Could not find any camera!");
     }
+
+    println!("Found {} cameras", camera_index_list.len());
+
+    let mut active_cam = 2;
     loop {
+        let mut cam = VideoCapture::new(camera_index_list[active_cam], videoio::CAP_ANY)?;
+        if !VideoCapture::is_opened(&cam)? {
+            panic!("Could not open camera!");
+        }
         let mut frame = Mat::default();
         cam.read(&mut frame)?;
         if frame.size()?.width > 0 {
@@ -16,8 +27,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
         let key = highgui::wait_key(10)?;
         if key > 0 && key != 255 {
-            break;
+            if key >= 48 && key <= 47 {
+                active_cam = (key - 48) as usize % camera_index_list.len();
+                println!("Switching to camera {}", active_cam)
+            } else {
+                break;
+            }
         }
     }
     Ok(())
+}
+
+fn list_cameras() -> Result<Vec<i32>, Box<dyn Error>> {
+    let mut camera_list = Vec::with_capacity(MAX_CAM_INDEX as usize);
+    for index in 0..MAX_CAM_INDEX {
+        let cam = VideoCapture::new(index, videoio::CAP_ANY)?;
+        if VideoCapture::is_opened(&cam)? {
+            camera_list.push(index);
+        }
+    }
+    camera_list.shrink_to_fit();
+    Ok(camera_list)
 }
